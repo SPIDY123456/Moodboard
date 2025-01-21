0b95ed5c84e5a40d95c335f512a1e05cf89d331b
SPIDY123456 <73572033+SPIDY123456@users.noreply.github.com>
2025-01-21 11:54:02 +0530

    Delete backend directory

--- backend/routes/uploadRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { upload } = require('../controllers/uploadController');
-const { protect } = require('../middleware/authMiddleware');
-const router = express.Router();
-
-router.post('/upload', protect, upload);
-
-module.exports = router;
